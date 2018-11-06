eecf4110f482eba82e4bbbf9bcec4db8880ced15
Anawin Wongsadit <imonmac@hotmail.com>
2018-11-06 22:09:18 +0700

    Move spotify API as seperate files

---
+++ js/spotify.js
@@ -0,0 +1,53 @@
+var spotify = {
+    token: '',
+    tokenExp: 0,
+
+    getToken: function ($cb) {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/awz-ywc16/spotifyToken.php",
+            data: {
+                passkey: "LOCALSPOTIFYAPI"
+            },
+            dataType: "json",
+            success: function (response) {
+                if (response && response.access_token && response.expires_in) {
+                    spotify.token = response.access_token;
+                    spotify.tokenExp = new Date().getTime() + response.expires_in;
+                    if (typeof $cb != "undefined") {
+                        console.log("Got token!");
+                        $cb();
+                    }
+                }
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " + btoa(this.clientId + ':' + this.clientSecret));
+            }
+        });
+    },
+
+    ajax: function (method, endpoint, params, $cb) {
+        let ajaxFunc = (method, endpoint, params, $cb) => {
+            $.ajax({
+                type: method,
+                url: ((endpoint.indexOf("api.spotify.com") == -1) ? "https://api.spotify.com" : "") + endpoint,
+                data: params,
+                dataType: "json",
+                success: function (response) {
+                    if (typeof $cb != "undefined") $cb(response);
+                },
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Authorization", "Bearer " + spotify.token);
+                }
+            })
+        };
+        if (this.token.length == 0 || new Date().getTime() >= this.tokenExp) this.getToken(()=>ajaxFunc(method, endpoint, params, $cb));
+        else ajaxFunc(method, endpoint, params, $cb);
+    },
+    get: function (endpoint, params, $cb) {
+        this.ajax("GET", endpoint, params, $cb);
+    },
+    post: function (endpoint, params, $cb) {
+        this.ajax("POST", endpoint, params, $cb);
+    }
+};
