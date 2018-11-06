be7576449a8dc0b338e48a21a13833214a2d6714
Anawin Wongsadit <imonmac@hotmail.com>
2018-11-06 21:46:59 +0700

    Implement search feature

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var spotify = {
                 if (response && response.access_token && response.expires_in) {
                     spotify.token = response.access_token;
                     spotify.tokenExp = new Date().getTime() + response.expires_in;
-                    if(typeof $cb != "undefined") {
+                    if (typeof $cb != "undefined") {
                         console.log("Got token!");
                         $cb();
                     }
@@ -27,19 +27,21 @@ var spotify = {
     },
 
     ajax: function (method, endpoint, params, $cb) {
-        let ajaxFunc = (method, endpoint, params, $cb) => {$.ajax({
-            type: method,
-            url: ((endpoint.indexOf("api.spotify.com") == -1) ? "https://api.spotify.com" : "") + endpoint,
-            data: params,
-            dataType: "json",
-            success: function (response) {
-                if (typeof $cb != "undefined") $cb(response);
-            },
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("Authorization", "Bearer " + spotify.token);
-            }
-        })};
-        if(this.token.length==0 || new Date().getTime()>=this.tokenExp) this.getToken(ajaxFunc(method, endpoint, params, $cb));
+        let ajaxFunc = (method, endpoint, params, $cb) => {
+            $.ajax({
+                type: method,
+                url: ((endpoint.indexOf("api.spotify.com") == -1) ? "https://api.spotify.com" : "") + endpoint,
+                data: params,
+                dataType: "json",
+                success: function (response) {
+                    if (typeof $cb != "undefined") $cb(response);
+                },
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Authorization", "Bearer " + spotify.token);
+                }
+            })
+        };
+        if (this.token.length == 0 || new Date().getTime() >= this.tokenExp) this.getToken(()=>ajaxFunc(method, endpoint, params, $cb));
         else ajaxFunc(method, endpoint, params, $cb);
     },
     get: function (endpoint, params, $cb) {
@@ -53,33 +55,53 @@ var app = new Vue({
     el: '#app',
     data: {
         loading: 1,
-        tracks: {}
-    }, 
+        trackId: '',
+        tracks: {},
+    },
     methods: {
-        getArtist: function(artists){
+        getArtist: function (artists) {
             let a = [];
-            artists.forEach((artist)=>{
+            artists.forEach((artist) => {
                 a.push(artist.name);
             })
             return a.join(", ");
+        },
+        processTracks: function (tracks, isFeature) {
+            tracks.items.forEach((item) => {
+                if (item.track)
+                    app.$set(app.tracks, item.track.id, item.track);
+                else
+                    app.$set(app.tracks, item.id, item);
+            });
+            if (typeof isFeature == "undefined" || isFeature) {
+                spotify.get('/v1/audio-features', { ids: Object.keys(app.tracks).join(",") }, (resp) => {
+                    resp.audio_features.forEach((item) => {
+                        if (!app.tracks[item.id]) return false;
+                        app.$set(app.tracks[item.id], 'features', item);
+                    });
+                });
+            }
+        },
+        search: function (q) {
+
+            app.loading = 1;
+            app.tracks = {};
+            if (!q || q.length == 0) {
+                spotify.get('/v1/playlists/37i9dQZEVXbMnz8KIWsvf9/tracks', { limit: 10, fields: 'items(track(id,name,artists(name),popularity,explicit,href,album(name,href,images)))' }, (resp) => {
+
+                    app.processTracks(resp);
+                    app.loading = 0;
+                });
+            } else {
+                spotify.get('/v1/search', { q: q, type: 'track', limit: 10 }, (resp) => {
+                    
+                    app.processTracks(resp.tracks);
+                    app.loading = 0;
+                });
+            }
         }
     }
-    
+
 });
 
-spotify.getToken(()=>{
-    app.loading = 1;
-    spotify.get('/v1/playlists/37i9dQZEVXbMnz8KIWsvf9/tracks',{fields:'items(track(id,name,artists(name),popularity,explicit,href,album(name,href,images)))'},(resp)=>{
-        app.loading = 0;
-        resp.items.forEach((item)=>{
-            app.$set(app.tracks, item.track.id, item.track);
-        });
-        spotify.get('/v1/audio-features',{ids:Object.keys(app.tracks).join(",")},(resp)=>{
-            //console.log(resp);
-            resp.audio_features.forEach((item)=>{
-                app.$set(app.tracks[item.id], 'features', item);
-            });
-        })
-    });
-});
-
+app.search();
