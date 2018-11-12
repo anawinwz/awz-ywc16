2a80e5caf1fe12a3ec6673fd3b7c6ad49c68cb1b
Anawin Wongsadit <imonmac@hotmail.com>
2018-11-12 19:33:47 +0700

    Read header from general way

--- audioAnalysis.php
+++ audioAnalysis.php
@@ -1,7 +1,6 @@
 <?php
 $trackId=@$_GET['trackId'];
-$headers=apache_request_headers();
-if(empty($trackId) || !@preg_match("/^[0-9a-zA-Z]+$/",$trackId) || !isset($headers['Authorization'])) {
+if(empty($trackId) || !@preg_match("/^[0-9a-zA-Z]+$/",$trackId) || !isset($_SERVER['HTTP_AUTHORIZATION'])) {
 	@header_remove("Cache-Control");
 	@header("HTTP/1.1 404 Not Found");
 	die();
@@ -13,7 +12,7 @@ if(@file_exists($dataFilename)) {
 }
 
 $process = curl_init("https://api.spotify.com/v1/audio-analysis/".$trackId);
-curl_setopt($process, CURLOPT_HTTPHEADER, array("Authorization: ".$headers["Authorization"]));
+curl_setopt($process, CURLOPT_HTTPHEADER, array("Authorization: ".$_SERVER['HTTP_AUTHORIZATION']));
 curl_setopt($process, CURLOPT_TIMEOUT, 30);
 curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($process, CURLOPT_CONNECTTIMEOUT, 30); 
